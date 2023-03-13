5505b5a14f471b7d3ce377f9e6afbffb2a5bc91b
LatvianRaccoon <arturs.svikis@gmail.com>
2023-03-13 12:31:50 +0100

    Python Lists

---
+++ PythonLists/main.py
@@ -0,0 +1,29 @@
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+area = [hall, kit, liv, bed, bath]
+
+# Adapt list areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+# Print areas
+print(area)
+print(areas)
+
+# Print out house
+print(house)
+# Print out the type of house
+print(type(house))
