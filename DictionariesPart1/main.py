305f8ad0d7cc8a1a27fa93fe15c9d9a2d5bb4e39
LatvianRaccoon <arturs.svikis@gmail.com>
2023-03-14 13:26:45 +0100

    Dictionaries Part 1

---
+++ DictionariesPart1/main.py
@@ -0,0 +1,15 @@
+world = {'Afghanistan': 30.55, 'Albania': 2.77, 'Algeria': 39.21}
+print(world['Albania'])
+
+# From string in countries and capitals, create dictionary europe
+europe = {'spain': 'madrid', 'france': 'paris',
+          'germany': 'berlin', 'norway': 'oslo'}
+
+# Print europe
+print(europe)
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
