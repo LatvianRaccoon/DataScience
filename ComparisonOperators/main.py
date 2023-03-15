327e58397b018faf6c37b6eaf5889fd8fb44c494
LatvianRaccoon <arturs.svikis@gmail.com>
2023-03-15 13:08:07 +0100

    Comparison Operators

---
+++ ComparisonOperators/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+np_height = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
+np_weight = np.array([65.4, 59.2, 63.6, 88.4, 68.7])
+bmi = np_weight / np_height ** 2
+print(bmi)
+print(bmi > 23)
+print(bmi[bmi > 23])
+
+# Create arrays
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than or equal to 18
+print(my_house >= 18)
+
+# my_house less than your_house
+print(my_house < your_house)
