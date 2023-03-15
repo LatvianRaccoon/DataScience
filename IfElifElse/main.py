22f848c33e27c7a862b387ee49f9a013b32de851
LatvianRaccoon <arturs.svikis@gmail.com>
2023-03-15 14:01:52 +0100

    If, Elif, Else Statements

---
+++ IfElifElse/main.py
@@ -0,0 +1,19 @@
+# Define variables
+room = "kit"
+area = 14.0
+
+# if statement for room
+if room == "kit":
+    print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
+else:
+    print("looking around elsewhere.")
+
+# if statement for area
+if area > 15:
+    print("big place!")
+elif area > 10:
+    print("medium size, nice!")
+else:
+    print("pretty small.")
