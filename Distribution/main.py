40f002ecb4afc557a37070df52ca8fb5ec5b3631
LatvianRaccoon <arturs.svikis@gmail.com>
2023-03-21 14:57:32 +0100

    Distribution

---
+++ Distribution/main.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(123)
+final_tails = []
+for x in range(10000):
+    tails = [0]
+    for y in range(10):
+        coin = np.random.randint(0, 2)
+        tails.append(tails[y] + coin)
+    final_tails.append(tails[-1])
+print(final_tails)
+
+plt.hist(final_tails, bins=10)
+plt.show()
+
+all_walks = []
+for i in range(10):
+    random_walk = [0]
+    for x in range(100):
+        step = random_walk[-1]
+        dice = np.random.randint(1, 7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1, 7)
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Convert all_walks to NumPy array: np_aw
+np_aw = np.array(all_walks)
+
+# Plot np_aw and show
+plt.plot(np_aw)
+plt.show()
+
+# Clear the figure
+plt.clf()
+
+# Transpose np_aw: np_aw_t
+np_aw_t = np.transpose(np_aw)
+
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
+plt.show()
