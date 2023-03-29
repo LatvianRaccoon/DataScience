21ed2ecb9f5e0f5616e9ad3fb8bf3fd2af3fe12f
LatvianRaccoon <arturs.svikis@gmail.com>
2023-03-29 18:38:16 +0200

    Data copier

--- DataCleaning/main.py
+++ DataCleaning/main.py
@@ -1,9 +1,22 @@
+# Program prints out CSV format in IPython shell in DataCamp editor.
+# You need just make copy and paste from IPython shell to text editor and save as csv file
+
 '''
-for i in range(7):
-    ro = taxi_owners.loc[i, :].values.flatten().tolist()
+f = dataset name
+
+for i in range(1):
+    row = f.columns.tolist()
 
     result = ''
-    for j in ro:
+    for j in row:
+        result += str(j).replace(',', '') + ","
+    result = result.rstrip(',')
+    print(result.strip())
+for i in range(50):
+    row = f.loc[i, :].values.flatten().tolist()
+
+    result = ''
+    for j in row:
         result += str(j).replace(',', '') + ","
     result = result.rstrip(',')
     print(result.strip())
